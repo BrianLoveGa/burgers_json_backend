9d4b84d8c2822900ec6cbf0733ada5402916249c
Brian Loveless <Brian.loveless1@yahoo.com>
2020-01-05 17:22:02 -0500

    school staff and fam

--- models/Character.js
+++ models/Character.js
@@ -5,9 +5,14 @@ const CharacterSchema = new mongoose.Schema({
   age: Number,
   description: String,
   gender: String,
+  voice:String,
   image: String
 });
 
 const Character = mongoose.model("Character", CharacterSchema);
+
+// All info is from Jan 2020 Wiki pedia pages
+// https://en.wikipedia.org/wiki/Bob%27s_Burgers#Belcher_family
+// https://en.wikipedia.org/wiki/List_of_Bob%27s_Burgers_characters
 
 module.exports = Character;
